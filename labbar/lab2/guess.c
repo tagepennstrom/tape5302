2a18f620792f1c30d197d9addaa4e0dc0df81262
tagepennstrom <tagepennstrom@hotmail.com>
2024-09-05 12:55:07 +0200

    gjorde färdigt guess

--- labbar/lab2/guess.c
+++ labbar/lab2/guess.c
@@ -8,10 +8,40 @@ int main(void)
     char buf[buf_siz];
     int tal = random () % 1001;
     int spel = 1;
-    char namn;
+    int tries = 0;
+    int guess;
+    char *namn;
+
+
     while (spel == 1)
     {
         namn = ask_question_string("Skriv in ditt namn", buf, buf_siz);
+        printf("Du %s, jag tänker på ett tal ... " , namn);
+        guess = ask_question_int("kan du gissa vilket?");
+        while (tries <= 13 && guess != tal)
+        {
+            if (guess < tal)
+            {
+            guess = ask_question_int("Högre");
+            tries += 1;
+            }
+            else
+            {
+            guess = ask_question_int("Lägre");
+            tries += 1;
+            }
+
+        }
+        if (tal == guess)
+        {
+            spel = 0;
+            printf("Bingo!\nDet tog %s %d gissningar att komma fram till %d", namn, tries, tal);
+        }
+        else
+        {
+            spel = 0;
+            printf("Ajdå!\nNu har du slut på gissningar! Jag tänkte på %d!", tal);
+        }
     }
     return 0;
 }
--- labbar/lab2/utils.h
+++ labbar/lab2/utils.h
@@ -1,63 +1,10 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
-
 #include <stdio.h>
 
-void clear_input_buffer(void)
-{
-    int c;
-    do
-    {
-        c = getchar();
-    } while (c != '\n' && c != EOF);
-}
-
-int ask_question_int(char *question)
-{
-    int result;
-    int conversions;
-    do
-    {
-        printf("%s\n", question);
-        conversions = scanf("%d", &result);
-        clear_input_buffer();
-        putchar('\n');
-    }
-    while (conversions < 1);
-    return result;
-}
-
-int read_string(char *buf, int buf_siz)
-{
-    int counter = 0;
-    int c;
-    int i;
-    for (i = 0; i < buf_siz - 1; i++)
-    {
-        c = getchar();
-        if (c != '\n' && c != EOF)
-        {
-            buf[i] = c;
-            counter += 1;
-        }
-        else
-        {
-            buf[i] = '\0';
-            return counter;
-        }
-    }
-    return counter;
-}
-
-char *ask_question_string(char *question, char *buf, int buf_siz)
-{
-    int result = 0;
-    while(result == 0)
-    {
-    printf("%s\n", question);
-    result = read_string(buf, buf_siz);
-    }
-    return buf;  
-}
-
+void clear_input_buffer(void);
+int ask_question_int(char *question);
+int read_string(char *buf, int buf_siz);
+char *ask_question_string(char *question, char *buf, int buf_siz);
 #endif
+
