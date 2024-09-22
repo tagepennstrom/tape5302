dd0ece452e17e34f7fe9f801e9a06ceb5dcbc3fa
tagepennstrom <tagepennstrom@hotmail.com>
2024-09-22 23:37:54 +0200

    assignment1_step0

---
+++ assign1/hash_table.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "hash_table.h"
+
+typedef struct entry entry_t;
+
+struct entry
+{
+  int key;       // holds the key
+  char *value;   // holds the value
+  entry_t *next; // points to the next entry (possibly NULL)
+};
+
+struct hash_table
+{
+  entry_t *buckets[17];
+};
+
+ioopm_hash_table_t *ioopm_hash_table_create()
+{
+  /// Allocate space for a ioopm_hash_table_t = 17 pointers to
+  /// entry_t's, which will be set to NULL
+  ioopm_hash_table_t *result = calloc(1, sizeof(ioopm_hash_table_t));
+  return result;
+}
+
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
+  free(ht);
+}
